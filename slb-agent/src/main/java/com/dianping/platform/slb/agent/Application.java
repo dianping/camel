97d593245250bcac378b715e3c42d5ce2bc30764
leon.li <leon.li@dianping.com>
2015-12-03 23:47:57 +0800

    init slb-agent, use spring-boot

---
+++ slb-agent/src/main/java/com/dianping/platform/slb/agent/Application.java
@@ -0,0 +1,19 @@
+package com.dianping.platform.slb.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * dianping.com @2015
+ * slb - soft load balance
+ * <p/>
+ * Created by leon.li(Li Yang)
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
