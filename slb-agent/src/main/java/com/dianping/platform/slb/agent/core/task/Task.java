c587828ae1418ebd5b9cf499c9eba9b575e2b200
leon.li <leon.li@dianping.com>
2015-12-04 02:21:26 +0800

    add task

---
+++ slb-agent/src/main/java/com/dianping/platform/slb/agent/core/task/Task.java
@@ -0,0 +1,10 @@
+package com.dianping.platform.slb.agent.core.task;
+
+/**
+ * dianping.com @2015
+ * slb - soft load balance
+ * <p/>
+ * Created by leon.li(Li Yang)
+ */
+public interface Task {
+}
