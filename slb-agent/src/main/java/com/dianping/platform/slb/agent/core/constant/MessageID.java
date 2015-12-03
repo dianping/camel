d6b61d0d673bb0b24264c3a199d36e69eedad60e
leon.li <leon.li@dianping.com>
2015-12-03 23:50:01 +0800

    add MessageId

---
+++ slb-agent/src/main/java/com/dianping/platform/slb/agent/core/constant/MessageID.java
@@ -0,0 +1,23 @@
+package com.dianping.platform.slb.agent.core.constant;
+
+/**
+ * dianping.com @2015
+ * slb - soft load balance
+ * <p/>
+ * Created by leon.li(Li Yang)
+ */
+public enum MessageID {
+
+	SCRIPT_EXCUTE_EXCEPTION("script_execute_exception"), FILE_NOT_EXIST("file_not_exist"), DEFAULT("default");
+
+	private String m_messageId;
+
+	MessageID(String messageId) {
+		m_messageId = messageId;
+	}
+
+	public String getMessageId() {
+		return m_messageId;
+	}
+
+}
