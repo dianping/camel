9d019664760e83dfeabdf5cfc83eaf058e86e8a1
leon.li <leon.li@dianping.com>
2016-08-30 15:31:35 +0800

    remove unused code

---
+++ camel-agent/src/main/java/com/dianping/platform/slb/agent/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.dianping.platform.slb.agent.constant;
+
+/**
+ * dianping.com @2015
+ * slb - soft load balance
+ * <p/>
+ * Created by leon.li(Li Yang)
+ */
+public class Constants {
+
+	public static final String CHAREST_UTF8 = "utf-8";
+}
