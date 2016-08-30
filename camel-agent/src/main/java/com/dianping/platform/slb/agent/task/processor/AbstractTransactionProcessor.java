70476b5237db325d15c0f20b581c2288c2fc471b
leon.li <leon.li@dianping.com>
2016-08-30 17:14:56 +0800

    implement transaction processor

---
+++ camel-agent/src/main/java/com/dianping/platform/slb/agent/task/processor/AbstractTransactionProcessor.java
@@ -0,0 +1,128 @@
+package com.dianping.platform.slb.agent.task.processor;
+
+import com.dianping.platform.slb.agent.task.model.SubmitResult;
+import com.dianping.platform.slb.agent.transaction.Transaction;
+import com.dianping.platform.slb.agent.transaction.manager.TransactionManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * dianping.com @2015
+ * slb - soft load balance
+ * <p/>
+ * Created by leon.li(Li Yang)
+ */
+public abstract class AbstractTransactionProcessor implements TransactionProcessor {
+
+	private static final Logger m_logger = Logger.getLogger(AbstractTransactionProcessor.class);
+
+	protected BlockingQueue<Transaction> m_transactions = new LinkedBlockingDeque<Transaction>();
+
+	private AtomicReference<Transaction> m_currentTransaction = new AtomicReference<Transaction>();
+
+	private volatile boolean m_isTransactionDealerRunning = false;
+
+	@Autowired
+	protected TransactionManager m_transactionManager;
+
+	@Override
+	public SubmitResult submit(Transaction transaction) throws Exception {
+		m_logger.info("try to submit " + transaction);
+
+		SubmitResult submitResult = new SubmitResult(false);
+		final long txId = transaction.getTransactionID();
+
+		if (!m_transactionManager.startTransaction(txId)) {
+			m_logger.warn(String.format("transaction with same id exists, reject it %s", txId));
+			submitResult.setAccepted(false);
+			submitResult.setMessage("duplicate transaction id, please donot submit more than once.");
+		} else {
+			submitResult = addTransaction(transaction);
+		}
+
+		return submitResult;
+	}
+
+	protected SubmitResult addTransaction(Transaction transaction) {
+		SubmitResult result = new SubmitResult(false);
+
+		if (!m_transactions.offer(transaction)) {
+			result.setAccepted(false);
+			result.setMessage("put to queue failed");
+			return result;
+		}
+		result.setAccepted(true);
+		result.setMessage("queue size: " + m_transactions.size());
+		startTransactionDealer();
+		return result;
+	}
+
+	private void startTransactionDealer() {
+		if (!m_isTransactionDealerRunning) {
+			new Thread(new TransactionDealer()).start();
+		}
+	}
+
+	protected void runTransaction(Transaction transaction) throws Exception {
+		try {
+			m_currentTransaction.set(transaction);
+			transaction.setStatus(Transaction.Status.RUNNNG);
+			m_transactionManager.saveTransaction(transaction);
+
+			transaction.setStatus(doTransaction(transaction));
+		} finally {
+			m_currentTransaction.set(null);
+			try {
+				m_transactionManager.saveTransaction(transaction);
+			} catch (IOException e) {
+				m_logger.error("[save transaction error]" + transaction.getTransactionID());
+			}
+		}
+	}
+
+	protected abstract Transaction.Status doTransaction(Transaction transaction);
+
+	@Override
+	public boolean isTransactionProcessing(long transactionId) {
+		Transaction currentTransaction = m_currentTransaction.get();
+
+		if (currentTransaction.getTransactionID() == transactionId) {
+			return true;
+		}
+		return false;
+	}
+
+	class TransactionDealer implements Runnable {
+		@Override
+		public void run() {
+			m_isTransactionDealerRunning = true;
+
+			try {
+				while (true) {
+					Transaction transaction = m_transactions.poll(30, TimeUnit.SECONDS);
+
+					if (transaction == null) {
+						break;
+					} else {
+						try {
+							runTransaction(transaction);
+						} catch (Exception ex) {
+							m_logger.error("[run transaction error] id: " + transaction.getTransactionID());
+						}
+					}
+				}
+			} catch (InterruptedException e) {
+				m_logger.error("[extract transaction error]", e);
+			} finally {
+				m_isTransactionDealerRunning = false;
+			}
+		}
+	}
+
+}
---
+++ camel-agent/src/main/java/com/dianping/platform/slb/agent/task/processor/config/UpgradeTransactionProcessor.java
@@ -0,0 +1,58 @@
+package com.dianping.platform.slb.agent.task.processor.config;
+
+import com.dianping.platform.slb.agent.conf.ConfigureManager;
+import com.dianping.platform.slb.agent.task.processor.AbstractTransactionProcessor;
+import com.dianping.platform.slb.agent.task.workflow.engine.Engine;
+import com.dianping.platform.slb.agent.task.workflow.step.ConfigUpgradeStep;
+import com.dianping.platform.slb.agent.task.workflow.step.Step;
+import com.dianping.platform.slb.agent.transaction.Transaction;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * dianping.com @2015
+ * slb - soft load balance
+ * <p/>
+ * Created by leon.li(Li Yang)
+ */
+@Service
+public class UpgradeTransactionProcessor extends AbstractTransactionProcessor {
+
+	private final static Logger m_logger = Logger.getLogger(UpgradeTransactionProcessor.class);
+
+	@Autowired
+	private Engine m_engine;
+
+	@Override
+	protected Transaction.Status doTransaction(Transaction transaction) {
+		transaction.addProperty(ConfigureManager.PROPERTY_TRANSACTIONMANAGER, m_transactionManager);
+		int exitCode = -1;
+
+		try {
+			exitCode = m_engine.executeStep(ConfigUpgradeStep.INIT, transaction);
+		} catch (Exception ex) {
+			exitCode = Step.CODE_FAIL;
+			m_logger.error("[do transaction error]" + transaction.getTransactionID(), ex);
+		}
+		if (exitCode == Step.CODE_SUCCESS) {
+			return Transaction.Status.SUCCESS;
+		} else {
+			return Transaction.Status.FAILED;
+		}
+	}
+
+	@Override
+	public boolean cancel(long txId) {
+		if (isTransactionProcessing(txId)) {
+			m_engine.kill();
+			return true;
+		}
+		return false;
+	}
+
+
+
+}
