7324fe3f07e2f263798d0328aaf07f17afefd352
leon.li <leon.li@dianping.com>
2016-09-01 19:10:29 +0800

    fix a bug

--- camel-agent/src/main/resources/script/tengine_reload.sh
+++ camel-agent/src/main/resources/script/tengine_reload.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ `pgrep nginx | wc -l` != 0 ]; then
 	echo "reload tengine config"
-	sudo -u root /etc/init.d/nginx reload || { echo "fail to reload tengine, exit code is $?"; exit 1; }
+	sudo -u root /usr/local/nginx -s reload || { echo "fail to reload tengine, exit code is $?"; exit 1; }
 else
 	echo "start tengine"
-	sudo -u root /etc/init.d/nginx start || { echo "fail to start tengine, exit code is $?"; exit 1; }
+	sudo -u root /usr/local/nginx -s start || { echo "fail to start tengine, exit code is $?"; exit 1; }
 fi
